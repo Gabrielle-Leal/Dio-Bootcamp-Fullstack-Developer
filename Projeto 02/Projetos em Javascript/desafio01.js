4ab2ace68dcc43cab109786f99df61e07353f696
Gabrielle Leal <gabrielle.leal773@gmail.com>
2022-08-13 00:21:05 -0300

    Projeto 02

---
+++ Projeto 02/Projetos em Javascript/desafio01.js
@@ -0,0 +1,10 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// let employee = {};
+// employee.code = 10;
+// employee.name = "John";
+//RESPOSTA:
+const funcionario2 = {
+    codigo: 10,
+    nome: 'joao'
+};
